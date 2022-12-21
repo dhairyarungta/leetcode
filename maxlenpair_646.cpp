ca2d112940628e8591c53a6aa8fc82eb46d19a7f
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2022-12-21 14:35:58 +0530

    new solution

---
+++ maxlenpair_646.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+
+    static bool cmpFun( vector<int> &v1,  vector<int> &v2){
+        return (v1[1]<v2[1]);
+    }
+    int findLongestChain(vector<vector<int>>& pairs) {
+        sort(pairs.begin(),pairs.end(),cmpFun);
+        vector<int> lastPair = pairs[0];
+        int len = 1 ;
+
+        for (int i =1;i<pairs.size();i++){
+            if (pairs[i][0]>lastPair[1]){
+                len++;
+                lastPair = pairs[i];
+            }
+        }
+        return len;
+    }
+};
