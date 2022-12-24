065eb2e0e6ccabd5451651e6e1b3b18ad8e845ef
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2022-12-24 13:59:29 +0530

    update

--- max_subarray_53.cpp
+++ max_subarray_53.cpp
@@ -17,3 +17,36 @@ public:
         return globalMax;
     }
 };
+
+
+
+//code to print out the subarray as well
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int n = nums.size();
+
+        int globalMax= INT_MIN;
+        int curMax =0;
+        int st=0, en=0, s=0;
+        for (int i =0;i<n;i++){
+            curMax+=nums[i];
+            if(curMax>globalMax){
+                globalMax=curMax;
+                en = i;
+                st = s;
+
+            }
+            
+            if(curMax<0){
+                curMax =0;
+                s =i+1;
+            }
+        }
+        for(int i=st;i<=en;i++) cout<< nums[i]<<" ";
+        cout<<endl;
+
+        return globalMax;
+        
+        }
+};
