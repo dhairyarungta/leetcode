031c4194d4b3cdaf20f8358c36c77e109af870b4
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2022-12-26 16:59:22 +0530

    solution

---
+++ jump_game_55.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        bool check = false;
+        int i ;
+        vector<int> pos;
+
+        for ( i =0;i<nums.size();i++){
+            if(nums[i]==0)
+                pos.push_back(i);
+        }
+        if(pos.size()==0){
+            return true;
+        }
+
+        for(int m = 0;m<pos.size();m++){
+            int zeroIndex =pos[m];
+            if(zeroIndex ==nums.size()-1) return true;
+            
+            int endIndex = 0;
+            check = false;
+            for (int i = zeroIndex;i>=endIndex;i--){
+                if(nums[i]>zeroIndex-i){
+                    check =true;
+                }
+            } 
+            if(check==false){
+                return false;
+            }
+            endIndex=zeroIndex+1;
+        }
+
+    return check;
+    }
+
+
+};
