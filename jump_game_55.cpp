e28f323bfae34cc7317e879211e05b5300329e2f
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2022-12-26 17:26:51 +0530

    solution

--- jump_game_55.cpp
+++ jump_game_55.cpp
@@ -35,3 +35,16 @@ public:
 
 
 };
+
+
+//o(n) Single Scan
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxReach = 0;
+        for (int i =0;i<=maxReach && i<nums.size();i++){
+            maxReach = max(maxReach,i+nums[i]);
+        }
+        return (maxReach>=nums.size()-1);
+    }
+};
