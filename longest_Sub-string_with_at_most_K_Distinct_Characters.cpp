cc91877fc6c713b29741bce91d96cec925376700
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2022-12-28 15:08:50 +0530

    solution

---
+++ longest_Sub-string_with_at_most_K_Distinct_Characters.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+int getLengthofLongestSubstring(int k, string s)
+{  map<char, int> mp;
+   int n = s.size();
+   int types = 0;
+   int maxLen = INT_MIN;
+   int curLen = 0;
+   int j =0;
+   for (int i=0;i<n;i++){
+      if(mp[s[i]]!=0){
+        mp[s[i]]++;
+        maxLen = max(maxLen, i-j +1);
+      }
+      else if(mp[s[i]]==0 && types<k){
+         mp[s[i]]++; types++;
+         maxLen = max(maxLen, i-j+1 );
+      }
+      else {
+        while (types > k-1) {
+          mp[s[j]]--;
+          if (mp[s[j]] == 0)
+            types--;
+          j++;
+         }
+         mp[s[i]]++;
+         types++;
+      }
+   }
+   return maxLen;
+}
