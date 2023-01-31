decabfe13cea31cd5b4bcd9b1f391ee399074b3b
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-01-31 23:44:46 +0800

    solution

--- edit_distance_72.cpp
+++ edit_distance_72.cpp
@@ -1,4 +1,31 @@
+//DP solution
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
 
+        int ** dp = new int*[n+1];
+        for (int i =0;i<=n;i++)dp[i]= new int[m+1];
+
+        for (int i=0;i<=n;i++)dp[i][0]=i;
+        for (int j =0;j<=m;j++)dp[0][j]=j;
+
+        for (int i =1;i<=n;i++){
+            for (int j =1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                } 
+                else {
+                    dp[i][j]= 1+min({dp[i-1][j-1],dp[i-1][j],dp[i][j-1]});
+                }
+            }
+        }
+
+        return dp[n][m];
+
+    }
+};
 
 //Recursive solution
 // Exponential Time compelxity
