d37c9ad5e338b6173fe850ee4459518a785e62ae
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-02-01 21:20:14 +0800

    solution

---
+++ binary_tree_pruning_814.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+
+    bool recurPruneTree(TreeNode* root){
+        if(root == nullptr)
+            return false;
+
+        bool rightSubT = recurPruneTree(root->right);
+        bool leftSubT = recurPruneTree(root->left);
+        bool rootVal = rightSubT | leftSubT;
+        if (leftSubT==false )
+            root->left = nullptr;
+        if(rightSubT==false)
+            root->right = nullptr;
+
+        return rootVal|root->val;
+
+}
+    TreeNode* pruneTree(TreeNode* root) {        
+        recurPruneTree(root);
+        
+        if(root->val ==0 && root->left ==nullptr && root->right ==nullptr){
+            return nullptr;
+        }
+        
+        return root; 
+    }
+};
