c7b01d84b94ff346330ec6e05c101557acd04c45
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-02-02 16:41:39 +0800

    solution

--- pow_x_n_50.cpp
+++ pow_x_n_50.cpp
@@ -1,8 +1,32 @@
 // O(logn) time compelxity
 // binomial exponentiation
 
+class Solution {
+public:
+    double recmyPow(double x, long long n) {
+        if(n==0)
+            return 1;
 
+        int setBit = n%2;
 
+        if(setBit==1){
+            return x*recmyPow(x*x,n/2);
+        }
+        else return recmyPow(x*x,n/2);
+        
+    }
+
+    double myPow(double x, int n){
+        n = (long long)n;
+        if(n==0)
+            return double(1);
+        double ans = recmyPow(x,abs(n));
+        if(n<0)
+            return  (1/ans);
+        else 
+        return ans;
+    }
+};
 
 
 // O(n) time comeplexity
