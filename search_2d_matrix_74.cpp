ff7a0a872bda32538af9eb70ca8948e0a7b6100d
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2023-02-03 12:15:35 +0800

    solution

--- search_2d_matrix_74.cpp
+++ search_2d_matrix_74.cpp
@@ -1,3 +1,41 @@
+// O(logn) time complexity 
+//O(1)space complexity
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        int start = 0;
+        int end = (m*n)-1;
+        int startcol = 0,endcol=m-1;
+        
+        while(start<=end){
+            int midcol = (startcol+endcol)/2;
+            int numCount = (start+end)/2;
+            int offset = numCount%n;
+
+            if(numCount/n>(midcol)){
+                midcol++;
+            }
+            cout<<matrix[midcol][offset]<<" ";
+            if(matrix[midcol][offset]==target){
+                return true;
+            }
+            else if(matrix[midcol][offset]<target){
+                start=numCount+1;
+                startcol = start/n;
+            }
+            else{
+                end = numCount-1;
+                endcol = end/n;
+            }
+        }
+        return false;
+
+    }
+};
+
 // O(n) time complexity
 // O(n) space complexity
 
@@ -14,25 +52,9 @@ public:
                 vec.push_back(matrix[i][j]);
             }
         }
-
         return binary_search(vec.begin(),vec.end(),target);
 
 
     }
 };
 
-// class Solution {
-// public:
-//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-//         int m = matrix.size();
-//         int n = matrix[0].size();
-
-//         int start = 0;
-//         int end = (m*n)-1;
-//         int startcol = 
-//         while(start<=end){
-//             int midcol = 
-//         }
-
-//     }
-// };
