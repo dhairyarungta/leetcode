27845dedbb3ca4d6145df8450540a0479911b4bb
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2024-03-18 21:20:34 +0800

    Update Q5

--- 5.cpp
+++ 5.cpp
@@ -42,3 +42,46 @@ public:
         return s.substr(pos,maxLen);
     }
 };
+// #include <string>
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        int n =s.size();
+        if (n==1)
+            return s;
+        int dp[1009][1009]={0};
+        for (int i =0 ;i<n;i++)
+            dp[i][i] = 1;
+        int row = 0;    
+        int col = 0;
+        for (int l = 2;l<=n;l++){
+            for (int i =0;i<=n-l;i++){
+                int j = i+l-1;
+                if(s[i]!=s[j]){
+                    dp[i][j] = -1;
+                }               
+                else{
+
+                    if(dp[i+1][j-1]==-1 && i+1!=j)
+                        dp[i][j] = -1;
+                    else
+                        dp[i][j] = 1;
+                }
+
+            }
+        }
+
+        for (int i =0;i<n;i++){
+            for (int j =0 ;j<n;j++){
+                // cout<<dp[i][j]<<" ";
+
+                if(dp[i][j]>0 && j-i>=col-row){
+                    col = j;
+                    row =i;
+                }
+            }
+            // cout<<endl;
+        }
+        return s.substr(row,col-row+1);
+    }
+};
