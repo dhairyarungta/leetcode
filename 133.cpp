ecc3d8026c133265442f340ff89d0e12ef54529e
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2024-04-07 21:51:44 +0800

    Add solution to Q133

---
+++ 133.cpp
@@ -0,0 +1,76 @@
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        
+        if(node == nullptr)
+        {
+            cout<<"hello\n";
+            return nullptr;
+        }
+            
+        Node* baseNode = new Node(1);
+        set<int>seen;
+        stack<Node*>st;
+        unordered_map<int,Node*>mp;
+        st.push(node);
+       
+        while(!st.empty()){
+            Node* curNode = st.top();
+            Node* newNode = nullptr;
+            if(!mp.contains(curNode->val)){
+
+                newNode = new Node(curNode->val);
+                mp[curNode->val] = newNode;
+            }
+            else 
+                newNode = mp[curNode->val];
+
+            st.pop();
+            seen.insert(curNode->val);
+            for(Node* node : curNode->neighbors){
+                if(!seen.contains(node->val)){
+                    st.push(node);
+                }
+                if(!mp.contains(node->val)){
+                    Node* tempNodeInsert = new Node(node->val);
+                    mp[node->val] = tempNodeInsert;
+                  //  (tempNodeInsert->neighbors).push_back(newNode);
+                }
+                vector<Node*>& ref= newNode->neighbors;
+                if(std::find(ref.begin(),ref.end(),mp[node->val])==ref.end())
+                    (newNode->neighbors).push_back(mp[node->val]);
+            }
+        }
+//        for(auto [i,node]: mp){
+//           cout<<i<<" "<<node->val<<'\n';
+//           for(auto neighbor : node->neighbors){
+//            cout<<neighbor->val<<" ";
+//           }
+//           cout<<'\n'; 
+//        }
+        return mp[1];
+
+    }
+};
