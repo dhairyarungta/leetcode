a1817597f6a02f39c4d187d243e3fd7704c08bb8
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2024-04-07 23:21:26 +0800

    Add another solution for Q133

---
+++ 133_2.cpp
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
+    unordered_map<Node*,Node*>mp;
+    Node* cloneGraph(Node* node) {
+       
+    if(node==nullptr)
+        return nullptr;
+    if(!mp.contains(node)){
+        Node* newNode = new Node(node->val);
+        mp[node] = newNode;
+    }
+    else {
+        return mp[node];
+    }
+    for(Node* neighNode : node->neighbors){
+        Node* tempNode = cloneGraph(neighNode);
+        (mp[node]->neighbors).push_back(tempNode);
+    }
+    return mp[node];
+
+    }
+};
