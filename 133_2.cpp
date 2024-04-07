31f9657ce3194bb65d90e28e70c28ac2ba3fc8da
dhairyarungta <97079960+dhairyarungta@users.noreply.github.com>
2024-04-08 00:02:11 +0800

    Add BFS solution Q133

--- 133_2.cpp
+++ 133_2.cpp
@@ -21,6 +21,60 @@ public:
 
 class Solution {
 public:
+    Node* cloneGraph(Node* node) {
+        if(node ==nullptr)
+            return node;
+        queue<Node*>q;
+        unordered_map<Node*,Node*>mp;
+        q.push(node);
+        while(!q.empty()){
+            Node* curNode = q.front();
+            q.pop();
+            Node *newNode = nullptr;
+            if(mp.contains(curNode)){
+                newNode = mp[curNode];
+            }
+            else {
+                newNode= new Node(curNode->val);
+                mp[curNode] = newNode;
+            }
+            for(Node* neighNode: curNode->neighbors) {
+                if(!mp.contains(neighNode)&& neighNode!=nullptr){
+
+                    q.push(neighNode);
+                    mp[neighNode] = new Node (neighNode->val);
+                }
+                (newNode->neighbors).push_back(mp[neighNode]);
+            }         
+        }
+        return mp[node];
+    }
+};
+
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+class Solution {
+public:
     unordered_map<Node*,Node*>mp;
     Node* cloneGraph(Node* node) {
        
